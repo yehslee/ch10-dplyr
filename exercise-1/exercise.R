04cee15461cf53755849c74cbbc4f0cd2cae63b2
yehslee <yehslee@uw.edu>
2018-04-11 16:38:25 -0700

    Half complete exercise 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -11,23 +11,24 @@ library(fueleconomy)
 
 # You should now have access to the `vehicles` data frame
 # You can use `View()` to inspect it
-
+View(vehicles)
 
 # Select the different manufacturers (makes) of the cars in this data set. 
 # Save this vector in a variable
-
+makes <- vehicles$make
 
 # Use the `unique()` function to determine how many different car manufacturers
 # are represented by the data set
-
+unique <- length(unique(makes))
 
 # Filter the data set for vehicles manufactured in 1997
-
+df <- vehicles[vehicles$year == 1997,]
 
 # Arrange the 1997 cars by highway (`hwy`) gas milage
 # Hint: use the `order()` function to get a vector of indices in order by value
 # See also:
 # https://www.r-bloggers.com/r-sorting-a-data-frame-by-the-contents-of-a-column/
+df <- df[order(df$hwy), ]
 
 
 # Mutate the 1997 cars data frame to add a column `average` that has the average
